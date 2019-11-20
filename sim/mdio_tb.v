9fabe84713725e4bb79e443b1aeebf5156803b21
vicg42 <vicg42@gmail.com>
2019-11-20 09:27:05 +0300

    mdio.vhd - edit

--- sim/mdio_tb.v
+++ sim/mdio_tb.v
@@ -20,6 +20,9 @@ initial begin
     // $dumpvars;
 
     mdio_start = 1'b0;
+    mdio_aphy = 0;
+    mdio_areg = 0;
+    mdio_txd = 0;
 
     rst = 1'b0;
     #500;
@@ -29,6 +32,10 @@ initial begin
 
     #500;
     mdio_start = 1'b1;
+    mdio_aphy <= 5'h06;
+    mdio_areg <= 5'h0B;
+    mdio_txd  <= 16'h8FFA;
+    mdio_dir  <= 1'b1; //1 -tx; 0 -rx
     @(posedge clk);
 
     #100;
@@ -50,6 +57,7 @@ eth_mdio #(
     .usr_txd(mdio_txd),
     .usr_rxd(mdio_rxd),
     .usr_done(),
+    .usr_busy(),
 
     .p_out_mdio_t(),
     .p_out_mdio(),
