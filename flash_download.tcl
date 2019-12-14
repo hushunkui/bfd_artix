0c217af88b22a37fcde6312aac8c1ddf3745c24b
vicg42 <vicg42@gmail.com>
2019-12-14 16:09:55 +0300

    flash_download.tcl - cleaup

--- flash_download.tcl
+++ flash_download.tcl
@@ -7,14 +7,13 @@ set flash_name s25fl128sxxxxxx0-spi-x1_x2_x4
 set flash_interface spix4
 set flash_size 16
 set flash_addr_firmware 0x0
-# set flash_addr_usr_data 0x00800000
 set flash_index 0
 
 #Set fpga param
 set fpga_index 0
 
 #Set file path
-set bit_file ./firmware/test_connect_zynq_const_0x2AAA.bit
+set bit_file ./firmware/bfd_artix_firmware.bit
 set mcs_file ./firmware/bfd_artix_firmware.mcs
 
 #Set server path
@@ -31,7 +30,7 @@ if {![file exist $bit_file]} {
 
 #convert bit file to mcs file
 write_cfgmem -force -format mcs -size $flash_size -interface $flash_interface -verbose \
-             -loadbit "up $flash_addr_firmware $bit_file" -file $mcs_file
+            -loadbit "up $flash_addr_firmware $bit_file" -file $mcs_file
 
 #download firmware to flash
 open_hw
@@ -43,18 +42,17 @@ refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] $fpga_index]
 create_hw_cfgmem -hw_device [lindex [get_hw_devices] $fpga_index] -mem_device [lindex [get_cfgmem_parts $flash_name] $flash_index]
 set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
 set_property PROGRAM.FILES [list "$mcs_file" ] [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
-#set_property PROGRAM.BPI_RS_PINS {none} [ get_property PROGRAM.HW_CFGMEM [current_hw_device ]]
 set_property PROGRAM.UNUSED_PIN_TERMINATION $unused_pins [ get_property PROGRAM.HW_CFGMEM [current_hw_device ]]
 set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [current_hw_device ]]
 set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [current_hw_device ]]
 set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [current_hw_device ]]
 set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [current_hw_device ]]
 
-if {![string equal [get_property PROGRAM.HW_CFGMEM_TYPE  [current_hw_device]] \
-  [get_property MEM_TYPE [get_property CFGMEM_PART [get_property PROGRAM.HW_CFGMEM [current_hw_device ]]]]] } { \
-puts "download firmware for flash programming..."
-create_hw_bitstream -hw_device [current_hw_device] [get_property PROGRAM.HW_CFGMEM_BITFILE [ current_hw_device]];
-program_hw_devices [current_hw_device];
+if {![string equal [get_property PROGRAM.HW_CFGMEM_TYPE [current_hw_device]] \
+    [get_property MEM_TYPE [get_property CFGMEM_PART [get_property PROGRAM.HW_CFGMEM [current_hw_device ]]]]] } { \
+        puts "download firmware for flash programming..."
+        create_hw_bitstream -hw_device [current_hw_device] [get_property PROGRAM.HW_CFGMEM_BITFILE [ current_hw_device]];
+        program_hw_devices [current_hw_device];
 }
 
 puts "flash programming..."
--- flash_file_generate.tcl
+++ flash_file_generate.tcl
@@ -7,7 +7,6 @@ set flash_name s25fl128sxxxxxx0-spi-x1_x2_x4
 set flash_interface spix4
 set flash_size 16
 set flash_addr_firmware 0x0
-# set flash_addr_usr_data 0x00800000
 
 #Set file path
 set bit_file ./firmware/bfd_artix_firmware.bit
@@ -21,4 +20,4 @@ if {![file exist $bit_file]} {
 
 #convert bit file to mcs file
 write_cfgmem -force -format mcs -size $flash_size -interface $flash_interface -verbose \
-             -loadbit "up $flash_addr_firmware $bit_file" -file $mcs_file
+            -loadbit "up $flash_addr_firmware $bit_file" -file $mcs_file
