cffc82917f24e7b264c1a569b8c0354d8d90a99c
vicg42 <vicg42@gmail.com>
2020-03-26 21:21:53 +0300

    засинхронизировал файл ./fpga_reg.v проекта ARTIX с ./fpga_reg.v проекта ZYNQ

--- src/fpga_reg.v
+++ src/fpga_reg.v
@@ -35,6 +35,14 @@
 `define UREG_FIRMWARE_DATE    32'h00
 `define UREG_FIRMWARE_TIME    32'h04
 `define UREG_CTRL             32'h08
+    `define UREG_CTRL_DMA_TX_TEST_START_BIT    0  //
+    // assign aurora_control_pwd = reg_ctrl[16];
+    // assign gt_rst = reg_ctrl[4];
+    // assign aurora_rst = reg_ctrl[5];
+    // assign artix_eth_num = reg_ctrl[10:8];
+    // assign zynq_eth_num = reg_ctrl[13:12];
+    // assign aurora_control_pwd = reg_ctrl[16];
+
 `define UREG_TEST0            32'h0C
 `define UREG_TEST1            32'h10
 
@@ -58,3 +66,11 @@
 `define UREG_AURORA_I_CTL_0   32'h48
 
 `define UREG_ETHPHY_TEST_PRM  32'h4C
+    `define UREG_ETHPHY_TEST_PRM_PKT_SIZE_OFFSET    0
+    `define UREG_ETHPHY_TEST_PRM_PKT_SIZE_MASK      16'hFFFF
+    `define UREG_ETHPHY_TEST_PRM_PAUSE_SIZE_OFFSET  16
+    `define UREG_ETHPHY_TEST_PRM_PAUSE_SIZE_MASK    8'hFF
+    `define UREG_ETHPHY_TEST_PRM_PKT_COUNT_OFFSET   24
+    `define UREG_ETHPHY_TEST_PRM_PKT_COUNT_MASK     8'hFF
+
+`define UREG_DEVICE_TEMP       32'h50
